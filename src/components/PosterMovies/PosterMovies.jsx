976a170802c5af60b356bd7a67ce2b4c8b844fa7
limematheuz <mat.rib.lima@gmail.com>
2024-08-15 00:32:23 +0200

    add styles; parametrized routes; new component that render images of mvies

--- src/components/PosterMovies/PosterMovies.jsx
+++ src/components/PosterMovies/PosterMovies.jsx
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from 'react';
 import "../PosterMovies/PosterMovies.css"
 
 const movieImages = [
-  "https://i.pinimg.com/originals/c6/fc/d8/c6fcd8ca16c4d943b1f284333cfd0efc.jpg",//hp
-  "https://wallpapers.com/images/featured/coco-y9omqvuggsgv90cp.jpg",//
-  "https://moviefilmsla.wordpress.com/wp-content/uploads/2018/10/venom_wallpaper_1920x1080_mf.jpg",//ven
-  "https://beatdigital.mx/wp-content/uploads/2024/04/portada-6-1.jpg",//joker
+  "https://s1.1zoom.me/big0/841/287634-Sepik.jpg",//hp
+  "https://wallpapers.com/images/featured/coco-y9omqvuggsgv90cp.jpg",//coco
+  "https://m.media-amazon.com/images/S/pv-target-images/64f8b240e7660e3fe4e774cc8496b7af41f17cc02225a5f6d5e1f6542eac9e95.jpg",//ven
+  "https://i.pinimg.com/originals/33/ff/20/33ff20650468428dfb4e0ac41e9a1c5f.jpg",//joker
   "https://i.pinimg.com/originals/8b/0f/be/8b0fbe82c3f5bb73eca9f557e8a9879c.jpg",//btman
-  "https://www.antaycine.cl/publish/slides/villanofavorito.png", //gru
+  "https://images4.alphacoders.com/130/thumb-1920-1306935.jpg",//macaco
+  "https://images.hdqwalls.com/wallpapers/spiderman-2-2004-8m.jpg",
+  "https://wallpapers.com/images/hd/1920-x-1080-deadpool-pceouzot37jcnqiz.jpg"//dead
 ];
 
 export default function PosterMovies() {
@@ -26,7 +28,6 @@ export default function PosterMovies() {
       <img
         src={movieImages[currentImageIndex]}
         alt="Movie Poster"
-        style={{ width: '100%', height: 'auto', objectFit: 'cover' }}
       />
       <span className='pm-first-black-span'></span>
       <span className='pm-second-black-span'></span>
