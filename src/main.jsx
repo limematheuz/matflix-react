a3d9adbcb0d06f9b750a3d46c32cb4fb5ccecf36
limematheuz <mat.rib.lima@gmail.com>
2024-08-26 00:15:27 +0200

    main.js changed

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-.render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
+  </StrictMode>,
 )
